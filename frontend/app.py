f5b093ba1e06fd1b902760bdddaefe91127cd2bc
Andreas <163583880+Andrewim00@users.noreply.github.com>
2026-03-05 22:49:56 -0300

    frontend inicial

--- frontend/app.py
+++ frontend/app.py
@@ -1,1 +1,18 @@
 import streamlit as st
+from requests import post
+
+def main() -> None:
+    frontend = Frontend()
+
+class Frontend():
+
+    def __init__(self):
+        
+        st.title("DataViewer")
+
+        file = st.file_uploader("Envie o CSV", type="csv")
+
+        if file:
+            pass
+
+
